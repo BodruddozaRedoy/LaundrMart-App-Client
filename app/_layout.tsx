ef9ae3f613f7df2d4bcdaf7a8248729225367769
BodruddozaRedoy <bodruddozaredoy@gmail.com>
2025-11-23 14:32:12 +0600

    update _layout.tsx

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -2,18 +2,21 @@ import { Stack } from "expo-router";
 import { GestureHandlerRootView } from "react-native-gesture-handler";
 import { KeyboardProvider } from "react-native-keyboard-controller";
 import "../global.css";
+import { SafeAreaProvider } from "react-native-safe-area-context";
 
 export default function RootLayout() {
-    return (
-        <KeyboardProvider>
-            <GestureHandlerRootView style={{ flex: 1 }}>
-            <Stack screenOptions={{ headerShown: false, animation: "fade" }}>
-              <Stack.Screen name="index" />
-              <Stack.Screen name="(auth)" />
-              <Stack.Screen name="(customer)" />
-              <Stack.Screen name="(mart)" />
+  return (
+    <SafeAreaProvider>
+      <KeyboardProvider>
+        <GestureHandlerRootView style={{ flex: 1 }}>
+          <Stack screenOptions={{ headerShown: false, animation: "fade" }}>
+            <Stack.Screen name="index" />
+            <Stack.Screen name="(auth)" />
+            <Stack.Screen name="(customer)" />
+            <Stack.Screen name="(mart)" />
           </Stack>
-      </GestureHandlerRootView>
-        </KeyboardProvider>
+        </GestureHandlerRootView>
+      </KeyboardProvider>
+    </SafeAreaProvider>
   );
 }
